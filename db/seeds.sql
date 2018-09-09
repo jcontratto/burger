73b2176fb918d8176459cc7b8fdc9de52b413cf1
Jcontratto <jmc22@hotmail.com>
2018-09-09 16:04:16 -0400

    db commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Works Burger');
+
+
+
