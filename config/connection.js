36230005c2ac4619d9be2215e958591bcd31dbf6
Jcontratto <jmc22@hotmail.com>
2018-09-16 15:12:30 -0400

    jaws commit

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,11 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -8,6 +13,7 @@ var connection = mysql.createConnection({
   password: "root",
   database: "burger_db"
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
